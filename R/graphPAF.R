646743a2420edd15ffd193b558a5323a4171ac8f
John Ferguson <john.ferguson@nuigalway.ie>
2023-12-14 13:17:11 +0000

    minor unimportant change

--- R/graphPAF.R
+++ R/graphPAF.R
@@ -3,7 +3,7 @@
 #' Estimation and display of various types of population attributable fraction and impact fractions.  As well as the usual calculations of attributable fractions and impact fractions, functions are provided for attributable fraction nomograms and fan plots, continuous exposures, for pathway specific population attributable fractions, and for joint, average and sequential population attributable fractions.
 #'
 #' @references
-#' Ferguson, J., O’Leary, N., Maturo, F., Yusuf, S. and O’Donnell, M., 2019. Graphical comparisons of relative disease burden across multiple risk factors. BMC medical research methodology, 19(1), pp.1-9
+#' Ferguson, J., O’Leary, N., Maturo, F., Yusuf, S. and O’Donnell, M., 2019. Graphical comparisons of relative disease burden across multiple risk factors. BMC medical research methodology,  19(1), pp.1-9
 #' @references
 #' Ferguson, J., Maturo, F., Yusuf, S. and O’Donnell, M., 2020. Population attributable fractions for continuously distributed exposures. Epidemiologic Methods, 9(1).
 #' @references
